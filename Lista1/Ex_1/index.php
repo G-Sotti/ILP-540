00e22c7c79b37a262ca562b24c44b41d9b1ad0c6
G-Sotti <gabriel_sotti@hotmail.com>
2022-03-05 10:42:16 -0300

    Ex1_Ok

--- Lista1/Ex_1/index.php
+++ Lista1/Ex_1/index.php
@@ -8,25 +8,25 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, word!</title>
+    <title>Pagamento!</title>
   </head>
   <body class="container">    
     <h1>Ex1 Troco</h1>
     <form method="post" action="resposta.php">
         <div class="row">
-            <div class="col-2">
+            <div class="col-3">
                 <label for="valprod" class="label-control">
                     informe o valor do produto:
                 </label>
-                <input type="number" name="valor1"
-                id="valor1" class="form-control"/>
+                <input type="number" name="valprod"
+                id="valpag" class="form-control"/>
             </div>
-            <div class="col-2">
+            <div class="col-3">
                 <label for="valpag" class="label-control">
                     informe o valor pago:
                 </label>
-                <input type="number" name="valor2"
-                id="valor2" class="form-control"/>
+                <input type="number" name="valpag"
+                id="valpag" class="form-control"/>
             </div>
         </div>
         <div class="row mt-3">
