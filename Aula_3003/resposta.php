f6e4e94425aa169c00625af29db497a08e85496f
G-Sotti <99368612+G-Sotti@users.noreply.github.com>
2022-03-30 20:51:16 -0300

    ok

--- Aula_3003/resposta.php
+++ Aula_3003/resposta.php
@@ -2,21 +2,20 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-  <title>Aula 30/03!</title>
+    <title>Aula 30/03!</title>
 </head>
 
 <body>
-  <h1>Sistema em PHP!</h1>
-  <?php
-    if($_POST){
+    <h1>Sistema em PHP!</h1>
+    <?php
+    if ($_POST) {
         $email = $_POST['email'];
         $senha = $_POST['senha'];
         //Abaixo a estrutra para criar um cookie nome + variável + tempo de duração (time() + (86400 * 1),"/") para 1 dia
@@ -24,39 +23,34 @@
         echo "Seja bem-vindo ".$_COOKIE['usuario'];*/
 
         session_start();
-        if(($email == "v@v.com") && ($senha == "123")){
+        if (($email == "v@v.com") && ($senha == "123")) {
             $_SESSION['usuario'] = $email;
             $_SESSION['acesso'] = true;
-            echo "Bem-vindo usuário ".$_SESSION['usuario'];
+            echo "Bem-vindo usuário " . $_SESSION['usuario'];
             echo '<br/><a href="sair.php">Sair</a>';
-        }
-        else {
+        } else {
             header('Location: index.php');
             exit;
         }
-    }
-    else{
+    } else {
         session_start();
-        if($_SESSION['acesso'] == true){
-            echo "Seja bem-vindo usuario ".$_SESSION['usuario'];
+        if ($_SESSION['acesso'] == true) {
+            echo "Seja bem-vindo usuario " . $_SESSION['usuario'];
             echo '<br/><a href="sair.php">Sair</a>';
-        }
-        else{
+        } else {
             header('Location: index.php');
             exit;
         }
     }
-  ?>
+    ?>
 
-  <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Optional JavaScript; choose one of the two! -->
 
-  <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-    crossorigin="anonymous"></script>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-  <!-- Option 2: Separate Popper and Bootstrap JS -->
-  <!--
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
