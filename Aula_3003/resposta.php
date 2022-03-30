a66f0cc51c662836f77a69ebe2e66043c2381fd8
G-Sotti <99368612+G-Sotti@users.noreply.github.com>
2022-03-30 20:43:30 -0300

    ok

--- Aula_3003/resposta.php
+++ Aula_3003/resposta.php
@@ -28,7 +28,7 @@
             $_SESSION['usuario'] = $email;
             $_SESSION['acesso'] = true;
             echo "Bem-vindo usuário ".$_SESSION['usuario'];
-            echo '<a href="sair.php">Sair</a>';
+            echo '<br/><a href="sair.php">Sair</a>';
         }
         else {
             header('Location: index.php');
@@ -39,7 +39,7 @@
         session_start();
         if($_SESSION['acesso'] == true){
             echo "Seja bem-vindo usuario ".$_SESSION['usuario'];
-            echo '<a href="sair.php">Sair</a>';
+            echo '<br/><a href="sair.php">Sair</a>';
         }
         else{
             header('Location: index.php');
