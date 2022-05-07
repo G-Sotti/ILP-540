cbf00163dad5535f00f512eabdfdd88418de433d
G-Sotti <gabriel_sotti@hotmail.com>
2022-05-06 22:08:54 -0300

    comiit

--- ProjetoPHP/bootstrap.php
+++ ProjetoPHP/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+
+
 require_once __DIR__ . "/vendor/autoload.php";
 
 //AQUI A GENTE RECUPERA O QUE O USUÁRIO DIGITOU E QUAL MÉTODO HTTP ELE UTILIZOU
@@ -16,6 +18,9 @@ $router->get("/ola-mundo", function(){
     return "Olá Mundo!";
 });
 
+$router->get('/exemplo','Aluno\ProjetoPhp\Controller\ExercicioController::exibir');
+
+$router->post("/exemplo-resultado", 'Aluno\ProjetoPhp\Controller\ExercicioController::exibirResultado');
 //ADICIONAR AS ROTAS VÁLIDAS ACIMA
 
 $result = $router->handler();
--- ProjetoPHP/src/Controller/ExercicioController.php
+++ ProjetoPHP/src/Controller/ExercicioController.php
@@ -8,7 +8,13 @@ class ExercicioController
 {
 
     public static function exibir(){
-        require_once("exemplo.php");
+        require_once("../src/View/exemplo.php");
+    }
+    public static function exibirResultado(){
+        $valor1 = $_POST['valor1'];
+        $valor2 = $_POST['valor2'];
+        $soma = $valor1 + $valor2;
+        require_once("../src/View/exemplo.php");
     }
 
 }
